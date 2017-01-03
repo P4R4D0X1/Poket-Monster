8b5a163c24184141ea4daa5a9b6dddf2aa7c0034
Paradox <paradoxone1@gmail.com>
2017-01-03 09:59:23 +0100

    creation de la classe monster

---
+++ poket_monster/monster.hpp
@@ -0,0 +1,30 @@
+#ifndef MONSTER_HPP_INCLUDE
+#define MONSTER_HPP_INCLUDE
+
+#include <iostream>
+#include <string>
+
+namespace State{
+	enum TYPE{
+		poisoned,
+		paralized,
+		burned
+	};
+}
+
+class CMonster{
+	public:
+		CMonster();
+		virtual ~CMonster();
+
+
+	private:
+		std::string m_name;
+		int m_hp;
+		int m_speed;
+		int m_attack;
+		int m_defense;
+		State::TYPE m_state;
+};
+
+#endif
