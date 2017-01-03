9f02841f8225c8eb8a08ac21f99bb9f884e13037
Paradox <paradoxone1@gmail.com>
2017-01-03 10:57:00 +0100

    class player

---
+++ poket_monster/player.hpp
@@ -0,0 +1,16 @@
+#ifndef PLAYER_HPP_INCLUDE
+#define PLAYER_HPP_INCLUDE
+
+#include <iostream>
+#include <string>
+
+class CPlayer{
+	public:
+		CPlayer();
+		virtual ~CPlayer();
+
+	protected:
+
+};
+
+#endif
