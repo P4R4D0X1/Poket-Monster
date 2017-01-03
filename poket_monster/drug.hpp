c44325d25d2bd5ededc0fb69bffa391c74dea092
aurorebc <bourlier@et.esiea.fr>
2017-01-03 11:07:36 +0100

    plantation de drug

---
+++ poket_monster/drug.hpp
@@ -0,0 +1,15 @@
+#ifndef DRUG_HPP_INCLUDE
+#define DRUG_HPP_INCLUDE
+
+#include "object.hpp"
+
+class CDrug : public CObject{
+	public:
+		CDrug();
+		virtual ~CDrug();
+
+	protected:
+		
+};
+
+#endif
