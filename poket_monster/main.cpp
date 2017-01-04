307f8be12af0570f3a587ce23e7e64c8a16f028a
Paradox <paradoxone1@gmail.com>
2017-01-04 16:53:32 +0100

    yey

--- poket_monster/main.cpp
+++ poket_monster/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
 
+#include "parse.hpp"
+
 /*
 *
 *	On fais une fonction getFourAttackByType() dans le parse
@@ -11,11 +13,17 @@
 *	coder la mecanique de jeu basique
 *
 */
+
+
 using namespace std;
 
 int main(int argc, char **argv){
 
+	CParse l_parse;
 
+	l_parse.parseMonsters("monsters.pkmn");
+	l_parse.parseAttack("attacks.pkmn");
+	l_parse.parseObjects("objects.pkmn");
 
 	system("pause");
 	return EXIT_SUCCESS;
