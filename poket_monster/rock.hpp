20d4ef7a148f599d5d2a4987b8acec46abc54038
Paradox <paradoxone1@gmail.com>
2017-01-04 09:31:33 +0100

    Ajout de hpMax

--- poket_monster/rock.hpp
+++ poket_monster/rock.hpp
@@ -6,7 +6,7 @@
 class CRock : public CMonster{
 	public:
 		CRock();
-		CRock(std::string p_name, int p_hp, int p_speed, int p_attack, int p_defense);
+		CRock(std::string p_name, int p_hp, int p_hpMax, int p_speed, int p_attack, int p_defense);
 		virtual ~CRock();
 
 		void hide(); //hides during 3 rounds
