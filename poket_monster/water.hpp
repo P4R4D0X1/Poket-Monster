789554bdfa25de4aec2ca2430a3966282110b13e
Paradox <paradoxone1@gmail.com>
2017-01-04 09:59:33 +0100

    Modification de la conf du projet

--- poket_monster/water.hpp
+++ poket_monster/water.hpp
@@ -9,8 +9,8 @@ class CWater : public CMonster{
 		CWater(std::string p_name, int p_hp, int p_hpMax, int p_speed, int p_attack, int p_defense, float p_flood, float p_fall);
 		virtual ~CWater();
 
-		virtual void attack(Monster::ATTACK p_attack, CMonster&  p_enemy);
-		virtual Attack::STATE applyDamage(Attack::TYPE p_attackType, int p_damage);
+		void attack(Monster::ATTACK_SLOT p_attack, CMonster&  p_enemy);
+		Attack::STATE applyDamage(Attack::TYPE p_attackType, int p_damage);
 		bool flood();
 
 	protected:
